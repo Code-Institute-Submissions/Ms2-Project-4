2189b9b37a136c0de58d289bd7ad55b35c058fbd
SdMusic <peter_freemantle@hotmail.com>
2021-02-07 19:13:34 +0000

    card shuffle and create board functions added on main js

--- assets/js/main.js
+++ assets/js/main.js
@@ -50,3 +50,25 @@ const cardArray = [
         img: "assets/images/franky.png"
     }
 ];
+
+//The Card Shuffle
+
+  cardArray.sort(() => 0.5 - Math.random());
+  const grid = document.querySelector(".grid");
+  const resultDisplay = document.querySelector("#result");
+  var cardsChosen = [];
+  var cardsChosenId = [];
+  var cardsWon = [];
+
+//Creating the Board
+
+  function createBoard() {
+    for (let i = 0; i < cardArray.length; i++) {
+      const card = document.createElement("img");
+      card.classList.add("hvr-float-shadow");
+      card.setAttribute("src", "assets/images/blank.jpg");
+      card.setAttribute("data-id", i);
+      card.addEventListener("mousedown", flipCard);
+      grid.appendChild(card);
+    }
+  }
