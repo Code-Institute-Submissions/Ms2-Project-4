483f9d8e3c6bb798bc8bca1ca6b947d29b398cd8
SdMusic <peter_freemantle@hotmail.com>
2021-02-07 19:12:57 +0000

    card array for lv 1,2 & 3 added

---
+++ assets/js/level3.js
@@ -0,0 +1,100 @@
+//The Cards
+
+const cardArray = [
+    {
+        name: "ace",
+        img: " assets/images/ace.png"
+    },
+    {
+        name: "luffy",
+        img: " assets/images/luffy.png"
+    },
+    {
+        name: "nami",
+        img: " assets/images/nami.png"
+    },
+    {
+        name: "chopper",
+        img: " assets/images/chopper.png"
+    },
+    {
+        name: "nico-robin",
+        img: " assets/images/nico-robin.png"
+    },
+    {
+        name: "franky",
+        img: " assets/images/franky.png"
+    },
+    {
+        name: "bellamy",
+        img: " assets/images/bellamy.png"
+    },
+    {
+        name: "crocodile",
+        img: " assets/images/crocodile.png"
+    },
+    {
+        name: "doruflamingo",
+        img: " assets/images/doruflamingo.png"
+    },
+    {
+        name: "foxy",
+        img: " assets/images/foxy.png"
+    },
+    {
+        name: "mihawk",
+        img: " assets/images/mihawk.png"
+    },
+    {
+        name: "kuro",
+        img: " assets/images/kuro.png"
+    },
+    {
+        name: "ace",
+        img: " assets/images/ace.png"
+    },
+    {
+        name: "luffy",
+        img: " assets/images/luffy.png"
+    },
+    {
+        name: "nami",
+        img: " assets/images/nami.png"
+    },
+    {
+        name: "chopper",
+        img: " assets/images/chopper.png"
+    },
+    {
+        name: "nico-robin",
+        img: " assets/images/nico-robin.png"
+    },
+    {
+        name: "franky",
+        img: " assets/images/franky.png"
+    },
+    {
+        name: "bellamy",
+        img: " assets/images/bellamy.png"
+    },
+    {
+        name: "crocodile",
+        img: " assets/images/crocodile.png"
+    },
+    {
+        name: "doruflamingo",
+        img: " assets/images/doruflamingo.png"
+    },
+    {
+        name: "foxy",
+        img: " assets/images/foxy.png"
+    },
+    {
+        name: "mihawk",
+        img: " assets/images/mihawk.png"
+    },
+    {
+        name: "kuro",
+        img: " assets/images/kuro.png"
+    },
+];
---
+++ assets/js/main.js
@@ -0,0 +1,52 @@
+//The Cards
+
+const cardArray = [
+    {
+        name: "ace",
+        img: "assets/images/ace.png"
+    },
+    {
+        name: "luffy",
+        img: "assets/images/luffy.png"
+    },
+    {
+        name: "nami",
+        img: "assets/images/nami.png"
+    },
+    {
+        name: "chopper",
+        img: "assets/images/chopper.png"
+    },
+    {
+        name: "nico-robin",
+        img: "assets/images/nico-robin.png"
+    },
+    {
+        name: "franky",
+        img: "assets/images/franky.png"
+    },
+        {
+        name: "ace",
+        img: "assets/images/ace.png"
+    },
+    {
+        name: "luffy",
+        img: "assets/images/luffy.png"
+    },
+    {
+        name: "nami",
+        img: "assets/images/nami.png"
+    },
+    {
+        name: "chopper",
+        img: "assets/images/chopper.png"
+    },
+    {
+        name: "nico-robin",
+        img: "assets/images/nico-robin.png"
+    },
+    {
+        name: "franky",
+        img: "assets/images/franky.png"
+    }
+];
